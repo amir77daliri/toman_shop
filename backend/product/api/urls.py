78cfdc604b7c694ce8e781fe90558c3763ad4fca
Amirhossein_Daliri <amirdalir0111@gmail.com>
2024-07-09 15:23:41 +0330

    Add refresh_token endpoint, refactor urls

--- backend/product/api/urls.py
+++ backend/product/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import ProductListCreateApiView, ProductRetrieveUpdateDestroyApiView
 
 urlpatterns = [
-    path('', ProductListCreateApiView.as_view()),
-    path('<int:pk>', ProductRetrieveUpdateDestroyApiView.as_view()),
+    path('', ProductListCreateApiView.as_view(), name='product_list_create'),
+    path('<int:pk>', ProductRetrieveUpdateDestroyApiView.as_view(), name='product_detail'),
 ]
 
